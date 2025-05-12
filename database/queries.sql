fe53a88c8e0ba44b1c3a1e611e7536e95dc090a9
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-05-11 22:19:35 -0300

    feat: Added new query commands

--- database/queries.sql
+++ database/queries.sql
@@ -3,3 +3,30 @@ SHOW DATABASES;
 USE investment_aggregator_springboot_db;
 
 SHOW TABLES;
+
+SELECT * FROM tb_accounts;
+
+SELECT 
+  BIN_TO_UUID(account_id) AS account_id,
+  description,
+  BIN_TO_UUID(user_id) AS user_id
+FROM tb_accounts;
+
+SELECT * FROM tb_billingaddress;
+
+SELECT
+    BIN_TO_UUID(account_id) AS account_id,
+    number,
+    street
+FROM tb_billingaddress;
+
+SELECT * FROM tb_users;
+
+SELECT
+    BIN_TO_UUID(user_id) as user_id,
+    creation_timestamp,
+    email,
+    password,
+    update_timestamp,
+    username
+FROM tb_users;
